4483d628193f337fb2ae54ed8eb2103189280ac6
Brenoferraz <brenof28@gmail.com>
2025-09-02 20:59:32 -0300

    comit aula

---
+++ src/main/java/br/uniesp/si/techback/model/Filme.java
@@ -0,0 +1,20 @@
+package br.uniesp.si.techback.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Filme {
+
+private Long id;
+private String titulo;
+private String genero;
+private int anoLacamento;
+
+
+        }
+
+
+
